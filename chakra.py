a8ec42b8a9ba4f2ad71d6f769376a6f5c63638fb
Fabien Maussion <fabien.maussion@uibk.ac.at>
2020-06-16 16:30:46 +0200

    add tributary mechanism to bu bed (#10)

--- chakra.py
+++ chakra.py
@@ -58,14 +58,33 @@ class WaterMassBalance(LinearMassBalance):
 
 
 def bu_tidewater_bed(gridsize=200, gridlength=6e4, widths_m=600,
-                     b_0=260, alpha=0.017, b_1=350, x_0=4e4, sigma=1e4):
+                     b_0=260, alpha=0.017, b_1=350, x_0=4e4, sigma=1e4,
+                     water_level=0, split_flowline_before_water=None):
 
+    # Bassis & Ultee bed profile
     dx_meter = gridlength / gridsize
     x = np.arange(gridsize+1) * dx_meter
-    bed_h = b_0 - alpha * x + b_1 * np.exp(-((x - x_0) / sigma)** 2)
+    bed_h = b_0 - alpha * x + b_1 * np.exp(-((x - x_0) / sigma)**2)
+    bed_h += water_level
     surface_h = bed_h
     widths = surface_h * 0. + widths_m / dx_meter
-    return [RectangularBedFlowline(dx=1, map_dx=dx_meter, surface_h=surface_h,
+
+    if split_flowline_before_water is not None:
+        bs = np.min(np.nonzero(bed_h < 0)[0]) - split_flowline_before_water
+        fls = [RectangularBedFlowline(dx=1, map_dx=dx_meter,
+                                      surface_h=surface_h[:bs],
+                                      bed_h=bed_h[:bs],
+                                      widths=widths[:bs]),
+               RectangularBedFlowline(dx=1, map_dx=dx_meter,
+                                      surface_h=surface_h[bs:],
+                                      bed_h=bed_h[bs:],
+                                      widths=widths[bs:]),
+               ]
+        fls[0].set_flows_to(fls[1], check_tail=False, to_head=True)
+        return fls
+    else:
+        return [
+            RectangularBedFlowline(dx=1, map_dx=dx_meter, surface_h=surface_h,
                                    bed_h=bed_h, widths=widths)]
 
 
@@ -349,7 +368,7 @@ class KCalvingModel(FlowlineModel):
             # CFL condition
             if not self.fixed_dt:
                 maxu = np.max(np.abs(u_stag))
-                if maxu > 0.:
+                if maxu > cfg.FLOAT_EPS:
                     cfl_dt = self.cfl_number * dx / maxu
                 else:
                     cfl_dt = dt
