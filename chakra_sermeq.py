a13abbd301302802be5c1650ea80e9840f1dec79
Lizz Ultee [air] <ehultee@umich.edu>
2020-06-24 16:54:54 -0400

    Add dimensional dLdt function
    Pull over dLdt functionality from "fancy SERMeQ" to decide changes in length given a time-varying climate input.

--- chakra_sermeq.py
+++ chakra_sermeq.py
@@ -10,7 +10,7 @@ Created on Mon Jun 15 13:55:02 2020
 
 import numpy as np
 import math
-from scipy import interpolate
+from scipy import interpolate, integrate
 from oggm import cfg
 
 ## Global constants
@@ -67,27 +67,28 @@ def balance_calving_rate(flux, width, thickness=100):
 class PlasticGlacier(object):
     """A calving glacier with shape defined by plastic yielding at terminus.
     """
-    def __init__(self, yield_strength=150e3, width=500, 
-                 basal_yield=150e3, ice_yield_type='constant'):
+    def __init__(self, yield_strength=150e3, ice_yield_type='constant',
+                 width=500, basal_yield=150e3, glen_A=3.0e-25):
         """Initialize the glacier
 
         Parameters
         ----------
-        x : array, optional
-            Positions x (m/L0) along the flowline. Default initialized with PlasticGlacier.
         yield_strength : float, optional
             Yield strength of ice (Pa). The default is 150e3.
+        ice_yield_type : string, optional
+            'constant' or 'variable' (Mohr-Coulomb) yield strength of ice.  Default constant.
         width : float, optional
             Width of the glacier (m). The default is 500.
         basal_yield : float, optional
             Yield strength of basal substrate (Pa). The default is 150e3.
-        ice_yield_type : string, optional
-            'constant' or 'variable' (Mohr-Coulomb) yield strength of ice.  Default constant.
+        glen_A : float, optional
+            Glen's A parameter (Pa s-1/3).  Default is 3.0e-25.
         """
         self.yield_strength = yield_strength
+        self.ice_yield_type = ice_yield_type
         self.width = width
         self.basal_yield = basal_yield
-        self.ice_yield_type = ice_yield_type
+        self.glen_A = glen_A
     
     def set_xvals(self, x):
         """
@@ -314,5 +315,88 @@ class PlasticGlacier(object):
             
         return xs, bs, ss, ucs
 
+    def dLdt_from_mb(self, profile, catchment_mb, terminus_mb=None, dL=5/L0, verbose=False):
+        """Compute terminus rate of advance/retreat given mass balance forcing
+        
+        Parameters
+        ----------
+        profile: NDarray
+            The current profile (x, surface, bed) as calculated by plastic_profile
+        catchment_mb : float
+            Net rate of ice gain or loss (m/a), spatially averaged over catchment
+        terminus_mb : float, optional
+            Mass balance nearest the terminus (m/a). Default None sets this to catchment value.
+        dL : float, optional
+            Step (m/L0) over which to find dHdL. Default is 5 m.
+        verbose: Boolean, optional
+            Whether to print component parts for inspection.  Default False.
+
+        Returns
+        -------
+        dLdt: float
+            Rate of terminus position change (m/a).
+        """
+        profile_length = max(profile[0]) # nondimensional
+        if terminus_mb is None:
+            terminus_mb = catchment_mb
+    
+        # Terminus quantities on given profile
+        se_terminus = profile[1][-1] *H0
+        bed_terminus = profile[2][-1] *H0
+        H_terminus = se_terminus - bed_terminus
+        Bghm_terminus = self.bingham_num(bed_elev=bed_terminus/H0, thick=H_terminus/H0)
+        Hy_terminus = self.balance_thickness(bed_elev=bed_terminus/H0, B=Bghm_terminus/H0) *H0
+        
+        # At adjacent grid point on given profile
+        se_adj = profile[1][-2] *H0
+        bed_adj = profile[1][-2] *H0
+        H_adj = se_adj - bed_adj
+        Bghm_adj = self.bingham_num(bed_elev=bed_adj/H0, thick=H_adj/H0)
+        Hy_adj = self.balance_thickness(bed_elev=bed_adj/H0, B=Bghm_adj/H0) *H0
+        
+        # Diffs
+        dx_term = abs(profile[0][-2] - profile[0][-1]) *self.L0 #should be ~2m in physical units
+        dHdx = (H_adj-H_terminus)/dx_term
+        dHydx = (Hy_adj-Hy_terminus)/dx_term
+        tau = Bghm_terminus * (self.rho_ice * self.g * self.H0**2 / self.L0) 
+        dUdx_terminus = cfg.SEC_IN_YEAR * self.glen_A * tau**3 
+        
+        # Compute dHdL around given terminus
+        bed_mindL = self.bed_function(profile_length-dL)/H0
+        se_mindL = self.balance_thickness(bed_mindL, Bghm_terminus) + bed_mindL
+        profile_mindL = self.plastic_profile(startpoint=profile_length-dL, 
+                                             hinit=se_mindL, 
+                                             endpoint=min(profile[0]))
+        H_mindL = np.array(profile_mindL[1]) - np.array(profile_mindL[2]) #array of ice thickness from profile
+        bed_plusdL = self.bed_function(profile_length+dL)/H0
+        se_plusdL = self.balance_thickness(bed_plusdL, Bghm_terminus) + bed_plusdL
+        profile_plusdL = self.plastic_profile(startpoint=profile_length+dL, 
+                                             hinit=se_plusdL, 
+                                             endpoint=min(profile[0]))
+        H_plusdL = np.array(profile_plusdL[1]) - np.array(profile_plusdL[2]) #array of ice thickness
+        dHdL_discrete = np.array(H_plusdL - H_mindL)/(2*dL)
+        Area_int = integrate.simps(dHdL_discrete) *H0
+        multiplier = 1 - (Area_int/H_terminus)
+        
+        # Group the terms
+        numerator = terminus_mb - dUdx_terminus*H_terminus + (catchment_mb*profile_length*dHdx/H_terminus)
+        denominator = dHydx - (dHdx*multiplier)
+        dLdt_viscoplastic = numerator/denominator
+        
+        if verbose:
+            print('For inspection on debugging - all should be DIMENSIONAL (m/a):')
+            print('profile_length={}'.format(profile_length))
+            print('se_terminus={}'.format(se_terminus))
+            print('bed_terminus={}'.format(bed_terminus))
+            print('Hy_terminus={}'.format(Hy_terminus))
+            print('dx_term={}'.format(dx_term))
+            print('Area_int={}'.format(Area_int))
+            print('Checking dLdt: terminus_mb = {}. \n H dUdx = {}. \n Ub dHdx = {}.'.format(terminus_mb, dUdx_terminus*H_terminus, catchment_mb*profile_length*dHdx/H_terminus)) 
+            print('Denom: dHydx = {} \n dHdx = {} \n (1-Area_int/H_terminus) = {}'.format(dHydx, dHdx, multiplier))
+            print('Viscoplastic dLdt={}'.format(dLdt_viscoplastic))
+        else:
+            pass
+        
+        return dLdt_viscoplastic
         
         
